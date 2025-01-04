109803d71b55192f502675f774d3fd2aae2cc6e1
gabimatatov <gavriel.matatov@veeva.com>
2025-01-04 23:11:58 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val chatViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        chatViewModel.text.observe(viewLifecycleOwner){
+            textView.text = it
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
